e4ec1cee0d262947156091961ede57d67b7df6c5
Maryan W. Berhe <maryanberhe@gmail.com>
2020-02-19 18:38:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -24,13 +24,13 @@ end
 def remove_element_from_end_of_array(array)
   colors = ["Blue", "Red", "Green", "Pink"]
   
-  colors.pop("what")
+  colors.pop
 end
   
 def remove_element_from_start_of_array(array)
   colors = ["Blue", "Red", "Green", "Pink"]
   
-  colors.shift("fun")
+  colors.shift
 end
 
 def retrieve_element_from_index(array, index_number)
