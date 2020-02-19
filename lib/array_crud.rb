5587857cd3b0a9d1296153cbd83084bc073d4989
Maryan W. Berhe <maryanberhe@gmail.com>
2020-02-19 18:08:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   ["Blue", "Red", "Green", "Pink"]
-   puts add_element_to_end_of_array.push("Orange")
+    add_element_to_end_of_array.push("Orange")
 end 
  
 def add_element_to_start_of_array(array, element)
