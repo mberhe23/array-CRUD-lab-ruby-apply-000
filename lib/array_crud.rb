b994df6bea2af4c781d7840f6c83ab98e0c86e9c
Maryan W. Berhe <maryanberhe@gmail.com>
2020-02-19 17:50:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,35 @@
 def create_an_empty_array
-  
+  [ ]
 end
 
 def create_an_array
-  
+  [" ", " ", " ", " "]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  ["Blue", "Red", "Green", "Pink"].push
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  ["Blue", "Red", "Green", "Pink"]
 end
 
 def remove_element_from_end_of_array(array)
-  
+  ["Blue", "Red", "Green", "Pink"]
 end
-
-def remove_element_from_start_of_array(array)
   
+def remove_element_from_start_of_array(array)
+  ["Blue", "Red", "Green", "Pink"]
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  ["Blue", "Red", "Green", "Pink"]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  ["Blue", "Red", "Green", "Pink"]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  ["Blue", "Red", "Green", "Pink"]
 end
