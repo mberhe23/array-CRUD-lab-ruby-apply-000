d28f14312736decd6941528b545fe1004630e300
Maryan W. Berhe <maryanberhe@gmail.com>
2020-02-19 19:32:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -30,7 +30,7 @@ end
 return
 colors.pop
   
-def remove_element_from_end_of_array(array)
+def remove_element_from_start_of_array(array)
   colors = ["wow", "Red", "Green", "Pink"]
    puts color.shift
 end
